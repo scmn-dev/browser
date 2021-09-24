986bbe11423a0a76d4ff1f2be0fae91341cc5761
abdfnx <64256993+abdfnx@users.noreply.github.com>
2021-09-24 15:10:51 +0300

    import `os/exec` package in `browser_windows.go`

--- browser_windows.go
+++ browser_windows.go
@@ -1,6 +1,9 @@
 package browser
 
-import "golang.org/x/sys/windows"
+import (
+	"os/exec"
+	"golang.org/x/sys/windows"
+)
 
 func openBrowser(url string) error {
 	return windows.ShellExecute(0, nil, windows.StringToUTF16Ptr(url), nil, nil, windows.SW_SHOWNORMAL)
